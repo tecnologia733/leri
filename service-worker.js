92ce335cf27b4d974f7019252d7b986964509959
tecnologia <77143340+tecnologia733@users.noreply.github.com>
2025-06-27 12:48:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
-const CACHE_NAME = 'lerinha-cache-v1';
-const ASSETS_TO_CACHE = [
-  './index.html',
-  './favicon.png',
-  './manifest.json'
+const CACHE_NAME = "lerinha-v1";
+const ASSETS = [
+  "./index.html",
+  "./favicon.png",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll(ASSETS_TO_CACHE)
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
